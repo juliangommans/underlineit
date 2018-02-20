cfd6a5d6feff90a4acb7ef923093442fa8b5c0b4
Julian Gommans <julian@fullscreen.io>
2018-02-21 11:54:12 +1300

    facebook link (oops)

--- src/pages/Contact.js
+++ src/pages/Contact.js
@@ -17,7 +17,7 @@ class Contact extends Component {
 
         <p>Email: underlineitnz@gmail.com</p>
         <p>Phone: 027 319 4240</p>
-        <p><a style={{color: 'black'}} href=''><Icon color='blue' name='facebook' /> Facebook</a></p>
+        <p><a style={{color: 'black'}} href='https://www.facebook.com/underlineitnz/'><Icon color='blue' name='facebook' /> Facebook</a></p>
 
       </Container>
     )
