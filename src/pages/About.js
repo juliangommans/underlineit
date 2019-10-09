6e462e66022cc6222856d1c929863eadd7fa29ea
Louisa-G <56323562+Louisa-G@users.noreply.github.com>
2019-10-09 13:28:42 +1300

    Update About.js

--- src/pages/About.js
+++ src/pages/About.js
@@ -10,7 +10,7 @@ class About extends Component {
         <Header as="h2">
           About
           <Subheader>
-            <p>Underline It is owned and operated by Louisa Gommans.  Louisa has many years of experience as a university academic, translator, author and commercial lawyer.  While practising law part time, Louisa also provides freelance editing, proofreading and writing services.</p>
+            <p>Underline It is owned and operated by Louisa Gommans.  Louisa has many years of experience as a commercial lawyer, researcher, translator, and writer.</p>
           </Subheader>
         </Header>
         <Container textAlign='left'>
@@ -20,10 +20,10 @@ class About extends Component {
             <li>BA Hons in Italian Language, Art History and European Studies (Victoria University)</li>
             <li>ATCL Performance Piano (Trinity College, London)</li>
             <li>Author of art crime related articles and a chapter in “Art Crime and Its Prevention: A Handbook”)</li>
-            <li>Choral singer (Orpheus Choir of Wellington, Supertonic Choir)</li>
+            <li>Choral singer (Orpheus Choir of Wellington, Supertonic Choir) and musician</li>
             <li>Founding Trustee and Secretary of the New Zealand Art Crime Research Trust</li>
-            <li>Involvement in many arts groups including Friends of the Dowse Committee, the C Art Trust</li>
-            <li>Former intern in the Karanga Aotearoa Repatriation Team at Te Papa</li>
+            <li>Involvement in many not-for-profit and charitable arts organisations</li>
+            <li>Former intern in the Karanga Aotearoa Repatriation Team at Te Papa Tongarewa Museum of New Zealand</li>
           </ul>
         </Container>
         <Container>
